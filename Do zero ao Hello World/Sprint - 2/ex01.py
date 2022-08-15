1687d6fd11cb9073848a21395f38c6ce4219fc45
Andre R M Conforti <andre.r.m.conforti@gmail.com>
2022-08-15 17:39:02 -0300

    sprint 2 iniciada

---
+++ Do zero ao Hello World/Sprint - 2/ex01.py
@@ -0,0 +1,6 @@
+fruta = ('caqui', 'maça', 'abacaxi', 'pera')
+restaurante = (('aroz', 'feijão', 'bife'), ('alface', 'tomate'), ('sorvete', 'bolo'))
+
+coquetel = fruta + restaurante
+
+print(coquetel[4][1])
